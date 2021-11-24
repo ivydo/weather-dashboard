e94bd4d4a510e9ba838997d7842dea628854112b
Ivy Do <ivy.do2011@gmail.com>
2021-11-24 09:49:21 -0500

    added second api in array

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -4,6 +4,7 @@ var searchButton = document.querySelector("#search-btn");
 var repoContainerEl = document.querySelector("#repos-container");
 
 var searchArr = [];
+var searchArrTwo = [];
 
 // search by city
 async function getCityDetails(event) {
@@ -12,7 +13,7 @@ async function getCityDetails(event) {
   console.log(searchInput, "searchInput");
   try {
     const cityWeatherRes = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}&units=imperial`
     );
     console.log("citywweatherres", cityWeatherRes);
     searchArr = cityWeatherRes.data;
@@ -22,11 +23,25 @@ async function getCityDetails(event) {
   }
   //  clear search input
   $("#forecast-form")[0].reset();
+  getMoreDetails();
   displayForecast();
 }
 
-// display city name,
-// the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// second api to get uv index, current date
+async function getMoreDetails() {
+  try {
+    const cityWeatherResTwo = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${searchArr.coord.lat}&lon=${searchArr.coord.lon}&units=imperial&appid=${apiKey}`
+    );
+    console.log("citywweatherrestwo", cityWeatherResTwo);
+    searchArrTwo = cityWeatherResTwo.data;
+  } catch (err) {
+    // handle error
+    console.log(err);
+  }
+}
+
+// display city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // function displayCurrentCity() {
 //   var cityName = searchArr.name;
 //   console.log(searchArr.name, "searchArrayName");
@@ -35,11 +50,32 @@ async function getCityDetails(event) {
 //   heading.innerHTML = cityName;
 // }
 
-// append city name, temp and humidity into col-1
+// displays and appends city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index into main-forecast containter
 
 function displayForecast() {
   var mainForcastDIV = document.querySelector("#main-forecast");
-  const cityNameElement = document.createElement("h2");
+
+  var weatherIconElement = document.createElement("img");
+  var cityNameElement = document.createElement("h2");
+  var tempElement = document.createElement("p");
+  var humidElement = document.createElement("p");
+  var windElement = document.createElement("p");
+  var uvElement = document.createElement("p");
+
+  weatherIconElement.innerHTML = `${searchArr.weather.icon}`;
   cityNameElement.innerText = `${searchArr.name}`;
-  mainForcastDIV.append(cityNameElement);
+  tempElement.innerText =
+    "Temperature: " + `${searchArr.main.temp}` + "° Farenheit";
+  humidElement.innerText = "Humidity: " + `${searchArr.main.humidity}` + "%";
+  windElement.innerText =
+    "Wind Speed: " + `${searchArr.wind.speed}` + " miles/hour";
+  uvElement.innerText = "UV Index: " + `${searchArr.main.uv}`;
+
+  mainForcastDIV.append(
+    cityNameElement,
+    tempElement,
+    humidElement,
+    windElement,
+    uvElement
+  );
 }
