ce259a76daaa3e2dd4bd017909af2358195d0aa3
Ivy Do <ivy.do2011@gmail.com>
2021-11-24 08:55:54 -0500

    created dynamic elements to generate in main-forecast

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -2,10 +2,10 @@
 const apiKey = "d5a31c547245680a0f00b2ffe15f45df";
 var searchButton = document.querySelector("#search-btn");
 var repoContainerEl = document.querySelector("#repos-container");
-var cityDataContainer = document.querySelector("#col-1")
 
 var searchArr = [];
 
+// search by city
 async function getCityDetails(event) {
   event.preventDefault();
   var searchInput = $("#search-input").val();
@@ -16,13 +16,30 @@ async function getCityDetails(event) {
     );
     console.log("citywweatherres", cityWeatherRes);
     searchArr = cityWeatherRes.data;
-    console.log("searchArr", searchArr);
   } catch (err) {
-    // handle error 
+    // handle error
     console.log(err);
   }
-      //  clear search input
-  searchInput.val = "";
-};
+  //  clear search input
+  $("#forecast-form")[0].reset();
+  displayForecast();
+}
 
+// display city name,
+// the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// function displayCurrentCity() {
+//   var cityName = searchArr.name;
+//   console.log(searchArr.name, "searchArrayName");
+//   const cityHeading = document.createElement("h2");
+//   var heading = document.getElementById("cityname-header");
+//   heading.innerHTML = cityName;
+// }
 
+// append city name, temp and humidity into col-1
+
+function displayForecast() {
+  var mainForcastDIV = document.querySelector("#main-forecast");
+  const cityNameElement = document.createElement("h2");
+  cityNameElement.innerText = `${searchArr.name}`;
+  mainForcastDIV.append(cityNameElement);
+}
